acca3850931302abbf6cc33cba683d23a7182c6d
zilen02 <zilenmodi@gmail.com>
2023-08-09 19:00:47 +0530

    Create: First Next App :)

---
+++ nextjs-basic-1/src/app/about/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <>
+      <h1>About Page</h1>
+    </>
+  );
+};
+
+export default Page;
---
+++ nextjs-basic-1/src/app/contact/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <>
+      <h1>Contact Page</h1>
+    </>
+  );
+};
+
+export default Page;
